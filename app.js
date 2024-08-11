4c2f6ecb4e0e6d621f6e3d497e00cd61d5afa918
Chetan <chetan95497@gmail.com>
2024-08-11 22:13:26 +0530

    Added initial files

---
+++ app.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const socket = require("socket.io");
+const http = require("http");
+const {Chess} = require("chess.js");
+const path = require("path");
+const { title } = require("process");
+
+const app = express();
+
+const server = http.createServer(app);
+const io = socket(server);
+
+const chess = new Chess();
+let players = {};
+let currentPlayer = "w";
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+
+app.get("/", (req, res) => {
+    res.render("index", {title: "Chess game"});
+})
+
+io.on("connection", function(uniqueSocket){
+    console.log("Connected")
+
+    if(!players.white){
+        players.white = uniqueSocket.id;
+        uniqueSocket.emit("playerRole", "w");
+    } else if(!players.black){
+        players.black = uniqueSocket.id;
+        uniqueSocket.emit("playerRole", "b");
+    } else{
+        uniqueSocket.emit("spectatorRole");
+    }
+
+    uniqueSocket.on("disconnect", function(){
+        if(uniqueSocket.id === players.white){
+            delete players.white;
+        }
+        else if(uniqueSocket.id === players.black){
+            delete players.black;
+        }
+    })
+    uniqueSocket.on("move", function(move){
+        try{
+            if(chess.turn() === 'w' && uniqueSocket.id !== players.white) return;
+            if(chess.turn() === 'b' && uniqueSocket.id !== players.black) return;
+            const result = chess.move(move);
+            if(result){
+                currentPlayer = chess.turn();
+                io.emit("move", move);
+                io.emit("boardState", chess.fen());
+            } else{
+                console.log("Invalid move : ", move);
+                uniqueSocket.emit("invalid move: ", move);
+            }
+
+        } catch(err){
+            console.log(err);
+            uniqueSocket.emit("Invalid move ", move);
+        }
+    })
+})
+
+server.listen(3000, function(){
+    console.log("listening on port 3000")
+})
